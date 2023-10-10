e52d388c107a646207ee70f26731ef7909969c7f
nasms018 <cjm1s@naver.com>
2023-10-10 18:54:44 +0900

    병합

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AppContextProvider } from 'context/AppContextProvider';
+
 import BBSNav from 'layout/BBSNav';
 import Footer from 'layout/Footer';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+
 
 
 
@@ -14,14 +13,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <AppContextProvider>
+
         <Router>
             <BBSNav></BBSNav>
             <App />
             <Footer></Footer>
         </Router>
-    </AppContextProvider>
+
 
 );
 
-reportWebVitals();
