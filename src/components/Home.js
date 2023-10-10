022361a015ec5b1f9fff64fbd3b9af404a0be617
nasms018 <cjm1s@naver.com>
2023-10-10 20:30:27 +0900

    일단 실행은 되게 프로바이더 미적용

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+      <p class="border border-danger-subtle">
+      
+      기본화면띄울 곳.</p>
+
+    </div>
+  )
+}
--- src/layout/BBSNav.js
+++ src/layout/BBSNav.js
@@ -2,7 +2,7 @@
 export default function BBSNav() {
   
   return (
-    <>
+    <>내브
     </>
   );
 }
