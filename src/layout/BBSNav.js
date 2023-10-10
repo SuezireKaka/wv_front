26cb70d80658aef641b01c022a4a924d9023c850
nasms018 <cjm1s@naver.com>
2023-10-10 18:37:16 +0900

    내브랑 푸터추가

---
+++ src/layout/BBSNav.js
@@ -0,0 +1,17 @@
+
+export default function BBSNav() {
+  
+  return (
+    <>
+    </>
+  );
+}
+
+/*
+ boardList.map(board=>(
+        <>&nbsp;&nbsp;
+        <Link className='badge bg-warning text-wrap' key={board.id} to={`/board/${board.id}`}>
+            {board.name}
+            </Link></>
+        ))
+*/
---
+++ src/router/BBSRouter.js
@@ -0,0 +1,25 @@
+import PostList from 'components/post/PostList';
+import PostListWithScroll from '../components/post/PostListWithScroll';
+import Home from 'components/Home';
+import PostDetail from 'components/post/PostDetail';
+import { Route, Routes } from 'react-router';
+import Register from 'components/Register';
+import MemberList from 'components/MemberList';
+import PostNew from 'components/post/PostNew';
+
+
+export default function BBSRouter() {
+    return (
+        <Routes> 
+          <Route path="/" element={ <Home /> } />
+          <Route path="/board/:id" element={ <PostList /> } />
+          {/*<Route path="/board/:id" element={ <PostListWithScroll /> } />*/}
+          <Route path="/post/:id" element={ <PostDetail /> } />
+          <Route path="/post/new/:boardId" element={ <PostNew /> } />
+
+          <Route path="/sign-up" element={ <Register /> } />
+          <Route path="/member_list/:ownerId" element={ <MemberList /> } />
+        </Routes>
+       
+      )
+}
