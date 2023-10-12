63e9e5e36152f4ec8bd3ef183981fe53ae3b8b3d
SuezireKaka <rwccm3241@naver.com>
2023-10-12 19:53:16 +0900

    로그인 폼 대략적인 준비 완료

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
-import LoginStage from "login/LoginStage";
 import Register from "login/Register";
 
 export default function Home() {
+
   return (
     <div>
-      <LoginStage/>
-      <Register />
       <p class="border border-danger-subtle">
       
       기본화면띄울 곳.</p>
--- src/layout/BBSNav.js
+++ src/layout/BBSNav.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import { Fetch } from "toolbox/Fetch";
+import { useContext } from "react";
+import AppContext from "context/AppContextProvider";
+import UserProfile from "login/UserProfile";
 
 
 export default function BBSNav() {
+  const { auth } = useContext(AppContext);
+  const roles = auth.roles ? auth.roles : [""];
+
+
   const boardListUri = `http://localhost:8080/bb/anonymous/listAll`;
-  return (
-    <><Link to="/">Home</Link>
+  return <>
+    <Link to="/">Home</Link>
     &nbsp;&nbsp;|
     <Fetch uri={boardListUri} renderSuccess={renderSuccess} />
-    </>
-  );
+    {!roles || roles[0] === ""
+    ? <Link to="/"><button style={{float: "right", marginRight: "10px"}}>로그인</button></Link>
+    : <UserProfile user={auth}/>
+    }
+  </>;
   
   function renderSuccess(boardList) {
     return <>
