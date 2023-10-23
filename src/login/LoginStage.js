abb402de9490ea0c6791017c86c243a4161d1fad
nasms018 <cjm1s@naver.com>
2023-10-23 19:31:34 +0900

    일단 커밋

--- src/login/LoginStage.js
+++ src/login/LoginStage.js
@@ -1,6 +1,10 @@
 import { useRef, useState, useEffect, useContext } from 'react';
 import AppContext from 'context/AppContextProvider';
 import { Link, useNavigate } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { InputGroup } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const LOGIN_URL = 'http://localhost:8080/party/anonymous/sign-in';
 
@@ -79,30 +83,35 @@ const LoginStage = () => {
             {error
             ? <p>{errorMessage}</p>
             : null}
-            <form onSubmit={signIn}>
-                login id : <input type="text"
+            
+            <Form onSubmit={signIn}>
+            <InputGroup className="mb-3" style={{width:"50%", alignContent:'center'}}>
+            <InputGroup.Text id="basic-addon1">login id</InputGroup.Text>
+                <Form.Control type="text"
                 id="loginId"
                 ref={loginIdRef}
                 autoComplete="off"
                 onChange={(e) => setLoginId(e.target.value)}
                 value={loginId}
                 required
-            />
+            /> </InputGroup><InputGroup className="mb-3" style={{width:"50%", Align:'center'}}>
             <br></br>
-            password : <input type="password"
+            <InputGroup.Text id="basic-addon1">password</InputGroup.Text>
+            <Form.Control type="password"
                 id="passWord"
                 ref={passWordRef}
                 onChange={(e) => setPassWord(e.target.value)}
                 value={passWord}
                 required
             />
+            </InputGroup>
             <br></br><br></br>
-            <button onClick={f => f}>로그인</button>
-        </form>
-        <Link to={"/agreement/"}><button>회원가입</button></Link>
+            <Button onClick={f => f}>로그인</Button>
+        </Form>
+        <Link to={"/agreement/"}><Button>회원가입</Button></Link>
         <br></br>
         
-        <button>외부 로그인</button>
+        <Button>외부 로그인</Button>
     </div>
 };
 
