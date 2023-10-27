a853090b5c87abca2199656adee2373556a32f51
nasms018 <cjm1s@naver.com>
2023-10-27 09:28:33 +0900

    일단 멤버리스트 어나니머스 제외

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -119,7 +119,7 @@ export default function Showcase() {
                                     {console.log(post)}
                                     <Card id={post?.id} style={{ width: '18rem' }} ><br />
                                         <Link style={{ textDecoration: "none", color: "black" }} to={`/series/${post.id}`} state={{ seriesId: post.id, post: state?.post, page: 1, boardId: state?.boardId }}>
-                                            <OriginalViewOne imgDtoList={post.listAttachFile} x="250" y="250" />
+                                            <OriginalViewOne imgDtoList={post.listAttachFile} x="250" y="auto" />
                                             <Card.Body>
                                                 <Card.Title>{post?.title}</Card.Title>
                                                 {/*<Card.Text>{post?.writer?.nick}</Card.Text> */}
