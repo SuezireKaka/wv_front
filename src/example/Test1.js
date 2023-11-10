0027b4ae18ca1d043284f8710e2b43c6420fc32c
nasms018 <cjm1s@naver.com>
2023-11-10 18:49:59 +0900

    일단 임시 저장

--- src/example/Test1.js
+++ src/example/Test1.js
@@ -9,27 +9,9 @@ import { useContext } from 'react';
 import AppContext from 'context/AppContextProvider';
 export default function Test1() {
 
- 
-        const [colors, setColors] = React.useState([]);
   return (
     <article>
-      <header>
-        <h3>체크박스 그룹</h3>
-      </header>
-      <CheckboxGroup
-        label="좋아하는 색깔은?"
-        values={colors}
-        onChange={setColors}
-      >
-        <Checkbox value="red" defaultChecked>빨강</Checkbox>
-        <Checkbox value="yellow">노랑</Checkbox>
-        <Checkbox value="green">초록</Checkbox>
-        <Checkbox value="blue">파랑</Checkbox>
-        <Checkbox value="violet" disabled>
-          보라
-        </Checkbox>
-      </CheckboxGroup>
-      <footer>[{colors.join(",")}]을 좋아하시군요!</footer>
+      테스트
     </article>
   );
 
