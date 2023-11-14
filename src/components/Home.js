6ee45b7d26303ba4d15d83a021778a48170e4f13
nasms018 <cjm1s@naver.com>
2023-11-14 15:46:02 +0900

    홈화면 안보이는거 해결

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,8 +19,8 @@ import HomePlus from "./HomePlus";
 
 export default function Home() {
   const [index, setIndex] = useState(0);
-  const seriesUri = `/work/anonymous/listAllSeries/0002/1`;
-  const seriesUri2 = `/work/anonymous/listAllSeries/0003/1`;
+  const seriesUri = `/work/anonymous/listAllSeries/0002/1/?genreId=`;
+  const seriesUri2 = `/work/anonymous/listAllSeries/0003/1?genreId=`;
   const postUri = `/work/anonymous/listAllPost/0000/1`;
   const txtSearch = useRef();
 
