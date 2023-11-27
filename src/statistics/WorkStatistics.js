7110a207fe1af828eb3ad935455f6fa8d0f94631
SuezireKaka <rwccm3241@naver.com>
2023-11-27 16:15:16 +0900

    조회수 통계 초회 성공

--- src/statistics/WorkStatistics.js
+++ src/statistics/WorkStatistics.js
@@ -4,6 +4,7 @@ import { InputGroup, Button, Form } from "react-bootstrap";
 import AppContext from "context/AppContextProvider";
 import axios from "api/axios";
 import { FaCircle } from "react-icons/fa";
+import { Chart } from "react-google-charts";
 
 export default function WorkStatistics() {
     const { auth } = useContext(AppContext);
@@ -13,10 +14,12 @@ export default function WorkStatistics() {
 
     const param = useParams();
 
-    const [data, setData] = useState([]);
+    const [hasRequested, setHasRequested] = useState(false);
+
+    const [data, setData] = useState([["날짜", "조회수"], [new Date(), 0]]);
     const [error, setError] = useState("");
 
-    const POINT_STYLE = {backgroundColor : "#55aaff"}
+    const POINT_STYLE = { backgroundColor: "#55aaff" }
 
     const uri = `/elastic/anonymous/listLatestReadOf/${param.seriesId}/${period}/sex_${selectedSex}-age_${selectedAge}`
 
@@ -29,7 +32,17 @@ export default function WorkStatistics() {
                     "x-auth-token": `Bearer ${auth?.accessToken}`
                 }
             });
-            console.log(response);
+            let buckets = response.data.aggregations.byDay.buckets;
+            let table = [["날짜", "조회수"]];
+            if (buckets.length === 0) {
+                table.push([new Date().setHours(0, 0, 0, 0), 0])
+            }
+            buckets.forEach(readCnt => {
+                table.push([new Date(readCnt.key_as_string + " 00:00:00"), readCnt.doc_count])
+            })
+            console.log("이걸 구글 차트한테 맡긴다는 거지?", table)
+            setData(table)
+            setHasRequested(true);
         } catch (err) {
             setError("Registration Failed");
         }
@@ -37,16 +50,26 @@ export default function WorkStatistics() {
 
     function onChange(e, callback = f => f) {
         callback(e.target.value);
+        setHasRequested(false);
+    }
+
+    function calcColor() {
+        return selectedSex === "male" ? "#335be8" : selectedSex === "female" ? "#ef8fd2" : "#e3ac1c";
+    }
+
+    function saltData(rawData) {
+        let result = [["날짜", "조회수"]];
+
     }
 
     return <>
-        <br/>
+        <br />
         <form>
             <InputGroup className="mb-3">
                 <InputGroup.Text id="basic-addon2" style={POINT_STYLE}>
                     기간
                 </InputGroup.Text>
-                <Form.Select aria-label="Default select example" onChange={(e) => {onChange(e, setPeriod)}}>
+                <Form.Select aria-label="Default select example" onChange={(e) => { onChange(e, setPeriod) }}>
                     <option value={7}>최근 7일</option>
                     <option value={30}>최근 30일</option>
                 </Form.Select>
@@ -61,7 +84,7 @@ export default function WorkStatistics() {
                         label="상관없음"
                         name="userSex"
                         type="radio"
-                        onChange={(e) => {onChange(e, setSelectedSex)}}
+                        onChange={(e) => { onChange(e, setSelectedSex) }}
                         value="any"
                         id={`inline-radio-2`}
                     />
@@ -74,7 +97,7 @@ export default function WorkStatistics() {
                         name="userSex"
                         type="radio"
                         value="male"
-                        onChange={(e) => {onChange(e, setSelectedSex)}}
+                        onChange={(e) => { onChange(e, setSelectedSex) }}
                         id={`inline-radio-1`}
                     />
                 </InputGroup.Text>
@@ -85,7 +108,7 @@ export default function WorkStatistics() {
                         label="여성"
                         name="userSex"
                         type="radio"
-                        onChange={(e) => {onChange(e, setSelectedSex)}}
+                        onChange={(e) => { onChange(e, setSelectedSex) }}
                         value="female"
                         id={`inline-radio-2`}
                     />
@@ -93,7 +116,7 @@ export default function WorkStatistics() {
                 <InputGroup.Text id="basic-addon2" style={POINT_STYLE}>
                     연령대
                 </InputGroup.Text>
-                <Form.Select aria-label="Default select example" onChange={(e) => {onChange(e, setSelectedAge)}}>
+                <Form.Select aria-label="Default select example" onChange={(e) => { onChange(e, setSelectedAge) }}>
                     <option value={"any"}>상관없음</option>
                     {["10", "20", "30"].map(age => {
                         return <option value={age}>{age}대</option>
@@ -102,7 +125,7 @@ export default function WorkStatistics() {
             </InputGroup>
         </form>
         <br />
-        <FaCircle color={selectedSex === "male" ? "#335be8" : selectedSex === "female" ? "#ef8fd2" : "#e3ac1c"}/>
+        <FaCircle color={calcColor()} />
         <br />
         <Button variant="outline-primary" onClick={query}>
             요청하기
@@ -110,7 +133,26 @@ export default function WorkStatistics() {
         <br />
         <p>period : {period}, sex : {selectedSex}, age : {selectedAge}</p>
         <br />
-        <p>여기가 꺾은선 그래프 생길 곳</p>
+        {hasRequested
+        ? <Chart width={1024} height={768}
+            chartType="LineChart"
+            data={data}
+            options={{
+                title: `최근 ${period}일 동안 `
+                    + `${selectedAge === "any" ? "" : selectedAge + "대 "}`
+                    + `${selectedSex === "any" ? "" : selectedSex === "male" ? "남성 " : "여성 "}`
+                    + `독자들의 조회 수`
+                ,
+                pointSize: 5,
+                trendlines: {
+                    0: {
+                        pointSize: 0,
+                    }
+                },
+                colors: [calcColor()]
+            }}
+        />
+        : ""}
     </>
 }
 
